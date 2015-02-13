eb5ac4b2020b59bfc67574390d2c724635bb14f9
Maurice van der Star <maurice@qball.nl>
2015-02-13 11:44:07 +0100

    Support for whereis as fallback for which

--- classes/Kohana/HTML2PDF.php
+++ classes/Kohana/HTML2PDF.php
@@ -12,7 +12,22 @@ class Kohana_HTML2PDF {
 	{
 		// Find the binary file
 		if ( ! $binary = trim(`which wkhtmltopdf`))
-			throw new Kohana_Exception("wkhtmltopdf must be installed on this system");
+		{
+			// No binary found, try through whereis (which sometimes fails on php..)
+			if ($whereis = trim(`whereis wkhtmltopdf`))
+			{
+				// Match binary
+				// Example output: wkhtmltopdf: /usr/local/bin/wkhtmltopdf
+				if (preg_match('/ \/.*?bin\/wkhtmltopdf/i', $whereis, $matches)) {
+						$binary = $matches[0];
+				}
+			}
+
+			if ( ! $binary)
+			{
+				throw new Kohana_Exception("wkhtmltopdf must be installed on this system");
+			}
+		}
 
 		// Create an instance of the wkhtml pdf geneartor
 		$wkhtml = new HTML2PDF_Document($binary);
